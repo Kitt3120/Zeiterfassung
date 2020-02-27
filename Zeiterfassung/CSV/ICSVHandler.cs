e9696b5039cb7cc3cb76844101066b2426f33404
Kitt3120 <kitt3120@aol.de>
2020-02-27 12:28:21 +0100

    Added ICSVHandler Interface
    Added ICSVHandler interface

---
+++ Zeiterfassung/CSV/ICSVHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeiterfassung.CSV
+{
+    internal interface ICSVHandler<T>
+    {
+        T Parse(string line);
+
+        string Revert(T obj);
+    }
+}
