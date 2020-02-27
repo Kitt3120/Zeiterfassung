a17473e8f764c5ce5998c4a270d022c452e89fe1
Kitt3120 <kitt3120@aol.de>
2020-02-27 09:32:16 +0100

    Constructor for Arbeitszeit
    Added a constructor for the Arbeitszeit class

--- Zeiterfassung/Models/Arbeitszeit/Arbeitszeit.cs
+++ Zeiterfassung/Models/Arbeitszeit/Arbeitszeit.cs
@@ -7,5 +7,12 @@ namespace Zeiterfassung.Models.Arbeitszeit
         public DateTime Datum { get; }
         public TimeSpan Zeitspanne { get; set; }
         public string Beschreibung { get; }
+
+        public Arbeitszeit(DateTime datum, TimeSpan zeitspanne, string beschreibung)
+        {
+            Datum = datum;
+            Zeitspanne = zeitspanne;
+            Beschreibung = beschreibung;
+        }
     }
 }
