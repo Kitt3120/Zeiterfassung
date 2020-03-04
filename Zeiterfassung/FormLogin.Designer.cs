e6f12f3f3cae5d7429b5259004e202e08487a90e
Kitt3120 <Kitt3120@aol.de>
2020-03-04 22:04:27 +0100

    Added FormLogin
    Added FormLogin to be opened as a popup for logging in.

---
+++ Zeiterfassung/FormLogin.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Zeiterfassung
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUser = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxUser = new System.Windows.Forms.ComboBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelUser
+            // 
+            this.labelUser.AutoSize = true;
+            this.labelUser.Location = new System.Drawing.Point(100, 45);
+            this.labelUser.Name = "labelUser";
+            this.labelUser.Size = new System.Drawing.Size(29, 13);
+            this.labelUser.TabIndex = 0;
+            this.labelUser.Text = "User";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Passwort";
+            // 
+            // comboBoxUser
+            // 
+            this.comboBoxUser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxUser.FormattingEnabled = true;
+            this.comboBoxUser.Location = new System.Drawing.Point(135, 45);
+            this.comboBoxUser.Name = "comboBoxUser";
+            this.comboBoxUser.Size = new System.Drawing.Size(186, 21);
+            this.comboBoxUser.TabIndex = 2;
+            this.comboBoxUser.SelectedIndexChanged += new System.EventHandler(this.comboBoxUser_SelectedIndexChanged);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(135, 72);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(186, 20);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Enabled = false;
+            this.buttonLogin.Location = new System.Drawing.Point(172, 104);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(429, 139);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.comboBoxUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Login";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormLogin_FormClosed);
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxUser;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
---
+++ Zeiterfassung/FormLogin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zeiterfassung.Models.Person;
+using Zeiterfassung.Storage;
+using Zeiterfassung.Utils;
+
+namespace Zeiterfassung
+{
+    public partial class FormLogin : Form
+    {
+        private bool _verified = false;
+
+        private Action<Person> _successAction;
+
+        public FormLogin(Action<Person> successAction)
+        {
+            _successAction = successAction;
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            Array.ForEach(StorageContainer.Get<Person>().Values, person => comboBoxUser.Items.Add(person));
+
+            if (comboBoxUser.Items.Count > 0)
+                comboBoxUser.SelectedIndex = 0;
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string password = textBoxPassword.Text;
+            Person user = (Person)comboBoxUser.SelectedItem;
+            if (string.IsNullOrWhiteSpace(password) || user == null)
+                return;
+
+            if (!HashUtils.Verify(password, user.PasswordHash))
+                MessageBox.Show("Das Passwort war ungültig!", "Ungültiges Passwort", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                _verified = true;
+                Close();
+                _successAction.Invoke((Person)comboBoxUser.SelectedItem);
+            }
+        }
+
+        private void RefreshButton()
+        {
+            buttonLogin.Enabled = (comboBoxUser.SelectedItem != null && !string.IsNullOrWhiteSpace(textBoxPassword.Text));
+        }
+
+        private void FormLogin_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (!_verified)
+                Environment.Exit(0);
+        }
+
+        private void comboBoxUser_SelectedIndexChanged(object sender, EventArgs e) => RefreshButton();
+
+        private void textBoxPassword_TextChanged(object sender, EventArgs e) => RefreshButton();
+    }
+}
