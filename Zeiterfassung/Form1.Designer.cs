26f207d01fa5ecb7105f6178c8327c868e972c44
Kitt3120 <Kitt3120@aol.de>
2020-03-04 22:06:23 +0100

    Main behaviour update
    Added PreLoad()
    Added LoadPersons()
    Added SavePersons()
    Added OnAuthenticated() as callback for the login popup
    Added Form1_Load() to dynamically load in the Positions into the ComboBox
    Added functionality to add new users.

--- Zeiterfassung/Form1.Designer.cs
+++ Zeiterfassung/Form1.Designer.cs
@@ -28,20 +28,274 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBoxPersonManagement = new System.Windows.Forms.GroupBox();
+            this.checkBoxPasswortZeigen = new System.Windows.Forms.CheckBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.comboBoxPosition = new System.Windows.Forms.ComboBox();
+            this.labelPosition = new System.Windows.Forms.Label();
+            this.labelGehalt = new System.Windows.Forms.Label();
+            this.textBoxGehalt = new System.Windows.Forms.TextBox();
+            this.labelGeburtsdatum = new System.Windows.Forms.Label();
+            this.textBoxGeburtsdatum = new System.Windows.Forms.TextBox();
+            this.labelPasswort = new System.Windows.Forms.Label();
+            this.textBoxPasswort = new System.Windows.Forms.TextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxVorName = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelVorname = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.groupBoxTimeManagement = new System.Windows.Forms.GroupBox();
+            this.splitContainerManagement = new System.Windows.Forms.SplitContainer();
+            this.groupBoxPersonManagement.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerManagement)).BeginInit();
+            this.splitContainerManagement.Panel1.SuspendLayout();
+            this.splitContainerManagement.Panel2.SuspendLayout();
+            this.splitContainerManagement.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBoxPersonManagement
+            // 
+            this.groupBoxPersonManagement.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxPersonManagement.Controls.Add(this.checkBoxPasswortZeigen);
+            this.groupBoxPersonManagement.Controls.Add(this.buttonAdd);
+            this.groupBoxPersonManagement.Controls.Add(this.comboBoxPosition);
+            this.groupBoxPersonManagement.Controls.Add(this.labelPosition);
+            this.groupBoxPersonManagement.Controls.Add(this.labelGehalt);
+            this.groupBoxPersonManagement.Controls.Add(this.textBoxGehalt);
+            this.groupBoxPersonManagement.Controls.Add(this.labelGeburtsdatum);
+            this.groupBoxPersonManagement.Controls.Add(this.textBoxGeburtsdatum);
+            this.groupBoxPersonManagement.Controls.Add(this.labelPasswort);
+            this.groupBoxPersonManagement.Controls.Add(this.textBoxPasswort);
+            this.groupBoxPersonManagement.Controls.Add(this.labelEmail);
+            this.groupBoxPersonManagement.Controls.Add(this.textBoxEmail);
+            this.groupBoxPersonManagement.Controls.Add(this.textBoxVorName);
+            this.groupBoxPersonManagement.Controls.Add(this.textBoxName);
+            this.groupBoxPersonManagement.Controls.Add(this.labelVorname);
+            this.groupBoxPersonManagement.Controls.Add(this.labelName);
+            this.groupBoxPersonManagement.Cursor = System.Windows.Forms.Cursors.Default;
+            this.groupBoxPersonManagement.Location = new System.Drawing.Point(3, 3);
+            this.groupBoxPersonManagement.Name = "groupBoxPersonManagement";
+            this.groupBoxPersonManagement.Size = new System.Drawing.Size(380, 251);
+            this.groupBoxPersonManagement.TabIndex = 0;
+            this.groupBoxPersonManagement.TabStop = false;
+            this.groupBoxPersonManagement.Text = "Personenmanagement";
+            this.groupBoxPersonManagement.Visible = false;
+            // 
+            // checkBoxPasswortZeigen
+            // 
+            this.checkBoxPasswortZeigen.AutoSize = true;
+            this.checkBoxPasswortZeigen.Location = new System.Drawing.Point(320, 99);
+            this.checkBoxPasswortZeigen.Name = "checkBoxPasswortZeigen";
+            this.checkBoxPasswortZeigen.Size = new System.Drawing.Size(59, 17);
+            this.checkBoxPasswortZeigen.TabIndex = 15;
+            this.checkBoxPasswortZeigen.Text = "Zeigen";
+            this.checkBoxPasswortZeigen.UseVisualStyleBackColor = true;
+            this.checkBoxPasswortZeigen.MouseDown += new System.Windows.Forms.MouseEventHandler(this.checkBoxPasswortZeigen_MouseDown);
+            this.checkBoxPasswortZeigen.MouseUp += new System.Windows.Forms.MouseEventHandler(this.checkBoxPasswortZeigen_MouseUp);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(6, 222);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(164, 23);
+            this.buttonAdd.TabIndex = 14;
+            this.buttonAdd.Text = "Anlegen";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // comboBoxPosition
+            // 
+            this.comboBoxPosition.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPosition.FormattingEnabled = true;
+            this.comboBoxPosition.Location = new System.Drawing.Point(123, 175);
+            this.comboBoxPosition.Name = "comboBoxPosition";
+            this.comboBoxPosition.Size = new System.Drawing.Size(251, 21);
+            this.comboBoxPosition.TabIndex = 13;
+            // 
+            // labelPosition
+            // 
+            this.labelPosition.AutoSize = true;
+            this.labelPosition.Location = new System.Drawing.Point(6, 179);
+            this.labelPosition.Name = "labelPosition";
+            this.labelPosition.Size = new System.Drawing.Size(44, 13);
+            this.labelPosition.TabIndex = 12;
+            this.labelPosition.Text = "Position";
+            // 
+            // labelGehalt
+            // 
+            this.labelGehalt.AutoSize = true;
+            this.labelGehalt.Location = new System.Drawing.Point(6, 152);
+            this.labelGehalt.Name = "labelGehalt";
+            this.labelGehalt.Size = new System.Drawing.Size(38, 13);
+            this.labelGehalt.TabIndex = 11;
+            this.labelGehalt.Text = "Gehalt";
+            // 
+            // textBoxGehalt
+            // 
+            this.textBoxGehalt.Location = new System.Drawing.Point(123, 149);
+            this.textBoxGehalt.Name = "textBoxGehalt";
+            this.textBoxGehalt.Size = new System.Drawing.Size(251, 20);
+            this.textBoxGehalt.TabIndex = 10;
+            // 
+            // labelGeburtsdatum
+            // 
+            this.labelGeburtsdatum.AutoSize = true;
+            this.labelGeburtsdatum.Location = new System.Drawing.Point(6, 126);
+            this.labelGeburtsdatum.Name = "labelGeburtsdatum";
+            this.labelGeburtsdatum.Size = new System.Drawing.Size(73, 13);
+            this.labelGeburtsdatum.TabIndex = 9;
+            this.labelGeburtsdatum.Text = "Geburtsdatum";
+            // 
+            // textBoxGeburtsdatum
+            // 
+            this.textBoxGeburtsdatum.Location = new System.Drawing.Point(123, 123);
+            this.textBoxGeburtsdatum.Name = "textBoxGeburtsdatum";
+            this.textBoxGeburtsdatum.Size = new System.Drawing.Size(251, 20);
+            this.textBoxGeburtsdatum.TabIndex = 8;
+            // 
+            // labelPasswort
+            // 
+            this.labelPasswort.AutoSize = true;
+            this.labelPasswort.Location = new System.Drawing.Point(6, 100);
+            this.labelPasswort.Name = "labelPasswort";
+            this.labelPasswort.Size = new System.Drawing.Size(50, 13);
+            this.labelPasswort.TabIndex = 7;
+            this.labelPasswort.Text = "Passwort";
+            // 
+            // textBoxPasswort
+            // 
+            this.textBoxPasswort.Location = new System.Drawing.Point(123, 97);
+            this.textBoxPasswort.Name = "textBoxPasswort";
+            this.textBoxPasswort.PasswordChar = '*';
+            this.textBoxPasswort.Size = new System.Drawing.Size(191, 20);
+            this.textBoxPasswort.TabIndex = 6;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(6, 74);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(32, 13);
+            this.labelEmail.TabIndex = 5;
+            this.labelEmail.Text = "Email";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(123, 71);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(251, 20);
+            this.textBoxEmail.TabIndex = 4;
+            // 
+            // textBoxVorName
+            // 
+            this.textBoxVorName.Location = new System.Drawing.Point(123, 45);
+            this.textBoxVorName.Name = "textBoxVorName";
+            this.textBoxVorName.Size = new System.Drawing.Size(251, 20);
+            this.textBoxVorName.TabIndex = 3;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(123, 19);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(251, 20);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // labelVorname
+            // 
+            this.labelVorname.AutoSize = true;
+            this.labelVorname.Location = new System.Drawing.Point(6, 48);
+            this.labelVorname.Name = "labelVorname";
+            this.labelVorname.Size = new System.Drawing.Size(49, 13);
+            this.labelVorname.TabIndex = 1;
+            this.labelVorname.Text = "Vorname";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(6, 22);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name";
+            // 
+            // groupBoxTimeManagement
+            // 
+            this.groupBoxTimeManagement.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxTimeManagement.Location = new System.Drawing.Point(3, 3);
+            this.groupBoxTimeManagement.Name = "groupBoxTimeManagement";
+            this.groupBoxTimeManagement.Size = new System.Drawing.Size(380, 251);
+            this.groupBoxTimeManagement.TabIndex = 1;
+            this.groupBoxTimeManagement.TabStop = false;
+            this.groupBoxTimeManagement.Text = "Arbeitszeitmanagement";
+            this.groupBoxTimeManagement.Visible = false;
+            // 
+            // splitContainerManagement
+            // 
+            this.splitContainerManagement.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainerManagement.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainerManagement.IsSplitterFixed = true;
+            this.splitContainerManagement.Location = new System.Drawing.Point(12, 12);
+            this.splitContainerManagement.Name = "splitContainerManagement";
+            // 
+            // splitContainerManagement.Panel1
+            // 
+            this.splitContainerManagement.Panel1.Controls.Add(this.groupBoxPersonManagement);
+            this.splitContainerManagement.Panel1MinSize = 50;
+            // 
+            // splitContainerManagement.Panel2
+            // 
+            this.splitContainerManagement.Panel2.Controls.Add(this.groupBoxTimeManagement);
+            this.splitContainerManagement.Panel2MinSize = 50;
+            this.splitContainerManagement.Size = new System.Drawing.Size(776, 257);
+            this.splitContainerManagement.SplitterDistance = 386;
+            this.splitContainerManagement.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 507);
+            this.Controls.Add(this.splitContainerManagement);
             this.Name = "Form1";
             this.Text = "Fehlzeitenerfassung";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxPersonManagement.ResumeLayout(false);
+            this.groupBoxPersonManagement.PerformLayout();
+            this.splitContainerManagement.Panel1.ResumeLayout(false);
+            this.splitContainerManagement.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerManagement)).EndInit();
+            this.splitContainerManagement.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.GroupBox groupBoxPersonManagement;
+        private System.Windows.Forms.GroupBox groupBoxTimeManagement;
+        private System.Windows.Forms.SplitContainer splitContainerManagement;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelVorname;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxVorName;
+        private System.Windows.Forms.Label labelGehalt;
+        private System.Windows.Forms.TextBox textBoxGehalt;
+        private System.Windows.Forms.Label labelGeburtsdatum;
+        private System.Windows.Forms.TextBox textBoxGeburtsdatum;
+        private System.Windows.Forms.Label labelPasswort;
+        private System.Windows.Forms.TextBox textBoxPasswort;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label labelPosition;
+        private System.Windows.Forms.ComboBox comboBoxPosition;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.CheckBox checkBoxPasswortZeigen;
     }
 }
 
--- Zeiterfassung/Form1.cs
+++ Zeiterfassung/Form1.cs
@@ -1,18 +1,148 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using Zeiterfassung.CSV;
 using Zeiterfassung.IO.DataProvider;
+using Zeiterfassung.IO.DataWriter;
 using Zeiterfassung.Models.Person;
+using Zeiterfassung.Storage;
+using Zeiterfassung.Utils;
 
 namespace Zeiterfassung
 {
     public partial class Form1 : Form
     {
+        private Person _user;
+
         public Form1()
         {
+            PreLoad();
+
             InitializeComponent();
+        }
+
+        private async void PreLoad()
+        {
+            await LoadPersons();
+            new FormLogin(OnAuthenticated).ShowDialog(this);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Position als Auswahl zur ComboBox hinzufügen und ersten Eintrag standardmäßig auswählen
+            Array.ForEach((Position[])Enum.GetValues(typeof(Position)), en => comboBoxPosition.Items.Add(en));
+            if (comboBoxPosition.Items.Count > 0)
+                comboBoxPosition.SelectedIndex = 0;
+        }
+
+        private void OnAuthenticated(Person person)
+        {
+            _user = person;
+            Text = $"Fehlzeitenerfassung (Eingeloggt als {person.ToString()})";
+
+            if (person.Position == Position.Manager)
+            {
+                splitContainerManagement.Panel1Collapsed = false;
+                groupBoxPersonManagement.Visible = true;
+
+                splitContainerManagement.Panel2Collapsed = false;
+                groupBoxTimeManagement.Visible = true;
+            }
+            else
+            {
+                splitContainerManagement.Panel1Collapsed = true;
+                groupBoxPersonManagement.Visible = false;
+
+                splitContainerManagement.Panel2Collapsed = false;
+                groupBoxTimeManagement.Visible = true;
+            }
+        }
+
+        private async Task LoadPersons()
+        {
+            Person[] persons = CSVHandlers.CSVHandlerPerson.ParseAll(await DataProviders.FileDataProvider.ProvideAsync("Assets/Personen.csv")).ToArray();
+            Storage<Person> personStorage = StorageContainer.CreateStorage<Person>("Personen"); //Potentially overwrite old storage only after successfull parsing
+            Array.ForEach(persons, person => personStorage.Add(person));
+        }
+
+        private async Task SavePersons() => await DataWriters.FileDataWriter.WriteAllAsync("Assets/Personen.csv", CSVHandlers.CSVHandlerPerson.RevertAll(StorageContainer.Get<Person>().Values));
+
+        private async void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string vorname, name, email, passwort;
+            double gehalt;
+            DateTime geburtsdatum;
+            Position position;
+
+            vorname = textBoxName.Text;
+            if (string.IsNullOrWhiteSpace(vorname))
+            {
+                MessageBox.Show("Bitte das Feld \"Name\" ausfüllen!", "Fehlende Angabe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            name = textBoxVorName.Text;
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                MessageBox.Show("Bitte das Feld \"Vorname\" ausfüllen!", "Fehlende Angabe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            email = textBoxEmail.Text;
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                MessageBox.Show("Bitte das Feld \"Email\" ausfüllen!", "Fehlende Angabe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            passwort = textBoxPasswort.Text;
+            if (string.IsNullOrWhiteSpace(passwort))
+            {
+                MessageBox.Show("Bitte das Feld \"Passwort\" ausfüllen!", "Fehlende Angabe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (!double.TryParse(textBoxGehalt.Text, out gehalt))
+            {
+                MessageBox.Show("Bitte das Feld \"Gehalt\" korrekt ausfüllen!", "Fehlerhafte Angabe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (!DateTime.TryParse(textBoxGeburtsdatum.Text, out geburtsdatum))
+            {
+                MessageBox.Show("Bitte das Feld \"Geburtsdatum\" korrekt ausfüllen!", "Fehlerhafte Angabe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (comboBoxPosition.SelectedItem == null)
+            {
+                MessageBox.Show($"Bitte eine der {Enum.GetValues(typeof(Position)).Length} möglichen Positionen auswählen!", "Fehlerhafte Angabe", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            position = (Position)comboBoxPosition.SelectedItem;
+
+            if (StorageContainer.Get<Person>().Values.Any(person => person.Name == vorname && person.Vorname == name))
+            {
+                MessageBox.Show("Eine Person mit diesem Namen existiert bereits!", "Halt Stop!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            StorageContainer.Get<Person>().Add(new Person(vorname, name, position, geburtsdatum, email, HashUtils.GenerateHash(passwort), gehalt));
+            await SavePersons();
+            MessageBox.Show("Person hinzugefügt!", "Erfolgreich", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void checkBoxPasswortZeigen_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBoxPasswort.PasswordChar = '\0';
+            checkBoxPasswortZeigen.Checked = true;
+        }
+
+        private void checkBoxPasswortZeigen_MouseUp(object sender, MouseEventArgs e)
+        {
+            textBoxPasswort.PasswordChar = '*';
+            checkBoxPasswortZeigen.Checked = false;
         }
     }
 }
