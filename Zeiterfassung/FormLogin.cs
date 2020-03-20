7f59eb27160371dd17f9b0a65d48175db1d2e24e
Kitt3120 <Kitt3120@aol.de>
2020-03-20 12:13:34 +0100

    Comments for FormLogin
    Added comments to FormLogin

--- Zeiterfassung/FormLogin.cs
+++ Zeiterfassung/FormLogin.cs
@@ -16,16 +16,25 @@ namespace Zeiterfassung
 {
     public partial class FormLogin : Form
     {
+        //Wenn false, schließt sich das ganze Programm, falls man FormLogin schließen sollte.
+        //Da dies nicht passieren soll, wenn man sich erfolgreich eingeloggt hat, wird _verified beim einloggen auf true gesetzt.
+        //Siehe FormLogin_FormClosed()
         private bool _verified = false;
 
+        //Callback, welches ausgeführt wird, wenn der Nutzer sich erfolgreich mit einem Account eingeloggt hat.
         private Action<Person> _successAction;
 
+        /// <summary>
+        /// Eine Form zum einloggen des Nutzers
+        /// </summary>
+        /// <param name="successAction">Callback, welches beim erfolgreichen Einloggen ausgeführt wird</param>
         public FormLogin(Action<Person> successAction)
         {
             _successAction = successAction;
             InitializeComponent();
         }
 
+        // Beim Laden der Form alle Personen zur Auswahl hinzufügen
         private void FormLogin_Load(object sender, EventArgs e)
         {
             Array.ForEach(StorageContainer.Get<Person>().Values, person => comboBoxUser.Items.Add(person));
@@ -33,6 +42,11 @@ namespace Zeiterfassung
             if (comboBoxUser.Items.Count > 0)
                 comboBoxUser.SelectedIndex = 0;
         }
+
+        /// <summary>
+        /// Überprüft, ob der Hash des eingegebenen Passworts mit dem des Passworts des ausgewählten Benutzers übereinstimmt.
+        /// Schließt, falls übereinstimmend, FormLogin und führt das Callback aus.
+        /// </summary>
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
@@ -60,6 +74,9 @@ namespace Zeiterfassung
             buttonLogin.Enabled = (comboBoxUser.SelectedItem != null && !string.IsNullOrWhiteSpace(textBoxPassword.Text));
         }
 
+        /// <summary>
+        /// Beendet beim schließen von FormLogin das gesamte Programm, sofern noch nicht eingeloggt
+        /// </summary>
         private void FormLogin_FormClosed(object sender, FormClosedEventArgs e)
         {
             if (!_verified)
